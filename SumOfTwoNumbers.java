bc72a972405fc6b8cfe3b2dcad6ba8ec6faf6026
swetha-Full <36259984+swetha-Full@users.noreply.github.com>
2018-03-15 15:22:01 +0530

    My first Commit

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,14 @@
+package git.Example.Full;
+
+public class SumOfTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a, b, c;
+		a = 10;
+		b = 20;
+		c = a + b;
+		System.out.println("sum = " + c);
+	}
+
+}
