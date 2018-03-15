d51dd6487cb9984d5cd8bfb29072bd0db6e41669
swetha-Full <36259984+swetha-Full@users.noreply.github.com>
2018-03-15 15:35:56 +0530

    My second commit

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -1,12 +1,18 @@
 package git.Example.Full;
 
+import java.util.Scanner;
+
 public class SumOfTwoNumbers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a, b, c;
-		a = 10;
-		b = 20;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("a: ");
+		
+	a=sc.nextInt();
+	System.out.println("b: ");
+		b = sc.nextInt();
 		c = a + b;
 		System.out.println("sum = " + c);
 	}
